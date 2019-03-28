6f30eac09c66597d313006b514bfe3bb2cdfd37d
Matt Bell <mappum@gmail.com>
2019-03-28 16:30:04 -0700

    Let consumer specify call handler

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-module.exports = function proxmise (get, set, path = []) {
+module.exports = function proxmise (get, call, path = []) {
   if (typeof get !== 'function') {
     throw Error('Must specify a "get" handler')
   }
 
-  // create Promise and pull out resolve/reject funcs
-  let funcs
-  let promise = new Promise((resolve, reject) => {
-    funcs = { resolve, reject }
-  })
+  let { promise, resolve, reject } = createPromise()
 
-  // override 'then' and 'catch' to detect if a property is
-  // the one the user wants to access
-  function wrap (method) {
-    return function (...args) {
-      // run getter
-      let res = get(path, funcs.resolve, funcs.reject)
+  let lastProperty = path[path.length - 1]
+  let isPromiseMethod =
+    lastProperty === 'then' ||
+    lastProperty === 'catch'
 
-      // if getter returned a Promise, hook it up to the
-      // proxmise
-      if (res instanceof Promise) {
-        res.then(funcs.resolve, funcs.reject)
-      }
-
-      // call the actual Promise method (then/catch)
-      // to attach the user's handler
-      return method.call(promise, ...args)
-    }
-  }
-
-  // return proxied Promise
-  return new Proxy(promise, {
-    get (obj, key) {
-      // use overriden Promise methods
-      if (key === 'then') return wrap(promise.then)
-      if (key === 'catch') return wrap(promise.catch)
-
+  // return proxied thennable function
+  let target = function () {}
+  return new Proxy(target, {
+    get (target, key) {
       // recursively wrap props
-      return proxmise(get, set, path.concat(key))
+      return proxmise(get, call, path.concat(key))
     },
 
     set () {
@@ -45,6 +24,41 @@ module.exports = function proxmise (get, set, path = []) {
 
     deleteProperty () {
       throw Error('This object is read-only')
+    },
+
+    apply (target, thisArg, args) {
+      if (isPromiseMethod) {
+        // run getter
+        let res = get(path.slice(0, -1), resolve, reject)
+
+        // if getter returned a Promise, hook it up to the
+        // proxmise
+        if (res instanceof Promise) {
+          res.then(resolve, reject)
+        }
+
+        // call the actual Promise method (then/catch)
+        // to attach the user's handler
+        return promise[lastProperty](...args)
+      }
+
+      if (call == null) {
+        throw Error('Cannot call property')
+      }
+
+      return call(path, ...args)
     }
+
+    // TODO: handle other traps
   })
+}
+
+function createPromise () {
+  // create Promise and pull out resolve/reject funcs
+  let funcs
+  let promise = new Promise((resolve, reject) => {
+    funcs = { resolve, reject }
+  })
+  let { resolve, reject } = funcs
+  return { promise, resolve, reject }
 }
